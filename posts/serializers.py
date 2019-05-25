a8c37cc8d63b07813a5bbcc4e7a039d8a63d1bd7
Bohdan Kokovych <kokovych@gmail.com>
2019-05-25 19:09:40 +0300

    ReadOnlyField for custom filed (author is user)

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,12 +3,12 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField(source='user')
+    author = serializers.ReadOnlyField(source='user.id')
 
     class Meta:
         model = Post
         fields = 'title', 'author', 'date'
 
-    def get_author(self, obj):
-        return obj.user.id
+    # def get_author(self, obj):
+    #     return obj.user.id
 
